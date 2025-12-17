245804548f180caa2987acb5a9cfa2073da3d9d0
Rohan Rai <92577040+Rohanrai540@users.noreply.github.com>
2025-12-17 17:56:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+const buttons = document.querySelectorAll(".btn");
+
+function stopSounds() {
+  sounds.forEach(sound => {
+    const audio = document.getElementById(sound);
+    if (audio) {
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  });
+}
+
+sounds.forEach(sound => {
+  const audio = document.createElement("audio");
+  audio.src = `sounds/${sound}.mp3`;
+  audio.id = sound;
+  document.body.appendChild(audio);
+});
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    if (button.classList.contains("stop")) {
+      stopSounds();
+    } else {
+      stopSounds();
+      const sound = button.innerText;
+      document.getElementById(sound).play();
+    }
+  });
+});
